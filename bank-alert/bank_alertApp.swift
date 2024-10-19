e81765f46932e3c692a96aa3e88191031e93bb9c
khoadoanduy <khoadoanduy05@gmail.com>
2024-10-19 13:50:31 -0500

    Initial Commit

---
+++ bank-alert/bank_alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  bank_alertApp.swift
+//  bank-alert
+//
+//  Created by Đoàn Khoa on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct bank_alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bank-alertTests/bank_alertTests.swift
@@ -0,0 +1,17 @@
+//
+//  bank_alertTests.swift
+//  bank-alertTests
+//
+//  Created by Đoàn Khoa on 10/19/24.
+//
+
+import Testing
+@testable import bank_alert
+
+struct bank_alertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
